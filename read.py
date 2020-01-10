dcbbaac3841f0552444fa639969240024a9e6db0
Sing Heung <singwai0510@gmail.com>
2020-01-10 16:19:31 +0800

    new

--- read.py
+++ read.py
@@ -6,15 +6,20 @@ with open ('reviews.txt','r') as f:
 		data.append(line)
 		count += 1
 		if count % 1000 == 0:
-			print (data)
-			
+			print(len(data))
 
 
-print(len(data))
+print('檔案讀取完成,總共有',len(data),'個檔案')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+	if sum_len % 10000 == 0:
+		print(sum_len)
+	
+print('留言的平均長度' , sum_len/len(data))
 
 
-print(data[0])
-print('--------------')
-print(data[1])
+
 
 
