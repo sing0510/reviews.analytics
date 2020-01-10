ebbfa7971bc093b3928a1f98ecc9f8be084989e0
Sing Heung <singwai0510@gmail.com>
2020-01-11 02:55:42 +0800

    add filter funtion

--- read.py
+++ read.py
@@ -19,7 +19,12 @@ for d in data:
 	
 print('留言的平均長度' , sum_len/len(data))
 
-
-
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+print(new[1])
 
 
